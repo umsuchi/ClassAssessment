869624971dc921986704636ea73ddb1e1aa86055
umsuchi <ummulmomeninsuchi@gmail.com>
2025-05-20 21:13:05 +0600

    Area Calculating of Different Shape

---
+++ Java/AreaCalculator.java
@@ -0,0 +1,19 @@
+package ClassAssessment.Java;
+import java.util.*;
+class AreaCalculator{
+    double calculateArea(double radius){return (Math.PI * radius * radius);}
+    double calculateArea(double length , double width){return (length * width);}
+    int calculateArea(int side){return (side * side);}
+    public static void main(String [] args){
+        Scanner o = new Scanner(System.in);
+        System.out.println("Radius ,, Length ,, Width ,, Side");
+        double rad = o.nextDouble();
+        double len = o.nextDouble();
+        double wid = o.nextDouble();
+        int si = o.nextInt();
+        AreaCalculator obj = new AreaCalculator();
+        System.out.println("Area of circle with radius "+rad+" is "+obj.calculateArea(rad));
+        System.out.println("Area of rectangle with length "+len+"and width "+wid+" is "+obj.calculateArea(len , wid));
+        System.out.println("Area of square with side "+si+" is "+obj.calculateArea(si));
+    }
+}
