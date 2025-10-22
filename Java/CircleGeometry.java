1e6223cd8cf489f68a1bc067b633eb0dc53fe3d4
umsuchi <ummulmomeninsuchi@gmail.com>
2025-10-23 00:25:28 +0600

    Calculating Area and Circumference of Circle

--- Java/CircleGeometry.java
+++ Java/CircleGeometry.java
@@ -8,13 +8,14 @@ class CircleGeometry {
         System.out.println("For radius "+rad+" , Area = "+Math.PI*rad*rad+" , Circumference = "+2*Math.PI*rad);
     }
     public static void main(String[] args) {
-        Scanner o = new Scanner(System.in);
-        CircleGeometry obj = new CircleGeometry();
-        System.out.print("Enter radius : ");
-        double r1 = o.nextDouble();
-        obj.compute(r1);
-        System.out.print("Enter radius : ");
-        double r2 = o.nextInt();
-        obj.compute(r2);
+        try (Scanner o = new Scanner(System.in)){
+            CircleGeometry obj = new CircleGeometry();
+            System.out.print("Enter radius : ");
+            double r1 = o.nextDouble();
+            obj.compute(r1);
+            System.out.print("Enter radius : ");
+            double r2 = o.nextInt();
+            obj.compute(r2);
+        }
     }
 }
