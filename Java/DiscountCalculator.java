7726c6b56cbdd3065fe1e00d2e6dc3ac1efe6f78
umsuchi <ummulmomeninsuchi@gmail.com>
2025-10-23 00:36:51 +0600

    Calculating Discount on Different Price

--- Java/DiscountCalculator.java
+++ Java/DiscountCalculator.java
@@ -8,17 +8,22 @@ class DiscountCalculator {
         return (price * disPer) / 100 ;
     }
     public static void main(String[] args) {
-        Scanner o = new Scanner(System.in);
-        DiscountCalculator obj = new DiscountCalculator();
-        System.out.print("Enter Price : ");
-        double p1 = o.nextDouble();
-        System.out.print("Enter Discount : ");
-        double d1 = o.nextDouble();
-        System.out.println("Discount on product with price : "+p1+" and "+d1+"% is "+obj.calculateDiscount(p1 , d1));
-        System.out.print("Enter Price : ");
-        int p2 = o.nextInt();
-        System.out.print("Enter Discount : ");
-        int d2 = o.nextInt();
-        System.out.println("Discount on product with price : "+p2+" and "+d2+"% is "+obj.calculateDiscount(p2 , d2));
+        try (Scanner o = new Scanner(System.in)){
+            DiscountCalculator obj = new DiscountCalculator();
+            System.out.print("Enter Price : ");
+            double p1 = o.nextDouble();
+            
+            System.out.print("Enter Discount : ");
+            double d1 = o.nextDouble();
+            
+            System.out.println("Discount on product with price : "+p1+" and "+d1+"% is "+obj.calculateDiscount(p1 , d1));
+            System.out.print("Enter Price : ");
+            
+            int p2 = o.nextInt();
+            System.out.print("Enter Discount : ");
+            
+            int d2 = o.nextInt();
+            System.out.println("Discount on product with price : "+p2+" and "+d2+"% is "+obj.calculateDiscount(p2 , d2));
+        }
     }
 }
