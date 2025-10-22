880d135f46b23f9c2d11de7a91e5561ef8d5a9fb
umsuchi <ummulmomeninsuchi@gmail.com>
2025-10-23 00:12:20 +0600

    Area Calculating of Different Shape

--- Java/AreaCalculator.java
+++ Java/AreaCalculator.java
@@ -1,5 +1,6 @@
 package ClassAssessment.Java;
 import java.util.*;
+import java.io.*;
 class AreaCalculator{
     double calculateArea(double radius){
         return (Math.PI * radius * radius);
@@ -11,15 +12,18 @@ class AreaCalculator{
         return (side * side);
     }
     public static void main(String [] args){
-        Scanner o = new Scanner(System.in);
-        System.out.println("Radius ,, Length ,, Width ,, Side");
-        double rad = o.nextDouble();
-        double len = o.nextDouble();
-        double wid = o.nextDouble();
-        int si = o.nextInt();
-        AreaCalculator obj = new AreaCalculator();
-        System.out.println("Area of circle with radius "+rad+" is "+obj.calculateArea(rad));
-        System.out.println("Area of rectangle with length "+len+"and width "+wid+" is "+obj.calculateArea(len , wid));
-        System.out.println("Area of square with side "+si+" is "+obj.calculateArea(si));
+        try (Scanner o = new Scanner(new File("input.txt"))){
+            System.out.println("Radius ,, Length ,, Width ,, Side");
+            double rad = o.nextDouble();
+            double len = o.nextDouble();
+            double wid = o.nextDouble();
+            int si = o.nextInt();
+            AreaCalculator obj = new AreaCalculator();
+            System.out.println("Area of circle with radius "+rad+" is "+obj.calculateArea(rad));
+            System.out.println("Area of rectangle with length "+len+"and width "+wid+" is "+obj.calculateArea(len , wid));
+            System.out.println("Area of square with side "+si+" is "+obj.calculateArea(si));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
