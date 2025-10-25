f1de37c81b4736234aa8526baaa9823e0bdb3f47
umsuchi <ummulmomeninsuchi@gmail.com>
2025-10-26 00:27:38 +0600

    Total Cost on Shopping Cart

--- Java/ShoppingCart.java
+++ Java/ShoppingCart.java
@@ -8,21 +8,30 @@ public class ShoppingCart {
         return (price*quan);
     }
     public static void main(String[] args) {
-        Scanner o = new Scanner(System.in);
-        ShoppingCart obj = new ShoppingCart();
-        System.out.print("Enter no of items : ");
-        int n = o.nextInt();
-        System.out.print("Cost per item : ");
-        double p = o.nextDouble();
-        System.out.println("Total cost for "+n+" items at "+p+" each is "+obj.calculateTotal(p, n));
-        System.out.print("Enter no of items : ");
-        int n1 = o.nextInt();
-        System.out.print("Cost per item : ");
-        double p1 = o.nextDouble();
-        System.out.print("Enter no of items : ");
-        int n2 = o.nextInt();
-        System.out.print("Cost per item : ");
-        double p2 = o.nextDouble();
-        System.out.println("Total cost for "+n1+" items at "+p1+" and "+n2+" items at "+p2+" each is "+obj.calculateTotal(p1, n1, p2, n2));
+        try(Scanner o = new Scanner(System.in)){;
+            ShoppingCart obj = new ShoppingCart();
+            System.out.print("Enter no of items : ");
+
+            int n = o.nextInt();
+            System.out.print("Cost per item : ");
+
+            double p = o.nextDouble();
+            System.out.println("Total cost for "+n+" items at "+p+" each is "+obj.calculateTotal(p, n));
+            System.out.print("Enter no of items : ");
+
+            int n1 = o.nextInt();
+            System.out.print("Cost per item : ");
+            
+            double p1 = o.nextDouble();
+            System.out.print("Enter no of items : ");
+            
+            int n2 = o.nextInt();
+            System.out.print("Cost per item : ");
+            
+            double p2 = o.nextDouble();
+            System.out.println("Total cost for "+n1+" items at "+p1+
+                                " and "+n2+" items at "+p2+" each is "+
+                                obj.calculateTotal(p1, n1, p2, n2));
+        }
     }
 }
