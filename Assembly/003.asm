3a78cdb8107b12808097ae9683943de125da0f3e
umsuchi <ummulmomeninsuchi@gmail.com>
2026-01-27 00:52:23 +0600

    Function 1 2 9

---
+++ Assembly/003.asm
@@ -0,0 +1,32 @@
+.model small
+.stack 100h
+.data
+m db "I'm a good girl.$"
+
+.code
+main proc
+    ; 1 -> single key input
+    ; 2 -> single character output
+    ; 9 -> character string output
+    
+    mov ax, @data
+    mov ds, ax
+    
+    mov ah, 9
+    lea dx, m
+    int 21h
+    
+    
+    mov ah, 1
+    int 21h
+    mov bl, al 
+    
+    mov ah, 2
+    mov dl, bl 
+    int 21h
+    
+    exit:
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
